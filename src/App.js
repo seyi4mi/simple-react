2e4a4abd6e59eddb65b6ca5843e0ce72812faef3
seyi4mi <seyi4mi99@gmail.com>
2021-09-08 06:31:45 +0800

    Just States

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
 import "./App.css"
 // Import Components
-import Nav from "./components/nav"
-import Tweets from "./components/Tweets"
 
 function App() {
 	//Write javascript here
+	const [counter, setCounter] = useState(0)
+	const [toggle, setToggle] = useState(false)
+
+	const incrementer = () => {
+		setCounter(counter + 1)
+		console.log(counter)
+	}
+
+	const toggler = () => {
+		setToggle((prev) => !prev)
+	}
 
 	return (
 		<div className="App">
-			<h1>Hello React</h1>
-			<div className="home">
-				<Nav />
-				<Tweets />
-			</div>
+			<h1 className={toggle ? "active" : ""}>Hello React</h1>
+			<h2>Counter {counter}</h2>
+			<button onClick={incrementer}>Click</button>
+			<button onClick={toggler}>Toogle</button>
 		</div>
 	)
 }
